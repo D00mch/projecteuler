dfd1ff6f53a6d6f4b80d63f9c687d863ed120099
liverm0r <arturdumchev@yandex.ru>
2019-04-10 19:13:51 +0300

    fix prime accuracy

--- src/projecteuler/problem_010.clj
+++ src/projecteuler/problem_010.clj
@@ -4,6 +4,6 @@
 (defn solution
   "Find the sum of all the primes below two million."
   []
-  (->> (utils/primes :accuracy 5)
+  (->> (utils/primes :accuracy 8)
        (take-while #(< % 2000000))
        (reduce + 0)))
